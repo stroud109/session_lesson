9efbfa868db97425851903a732c4f386df65b466
Student <user@chromebox-001.(none)>
2013-10-21 17:39:35 -0700

    first commit

--- app.py
+++ app.py
@@ -6,15 +6,44 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in!" % session['username']
+    else: 
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    username = model.authenticate(username, password)
+    if username != None:
+        flash("User authenticated!")
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
 
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/user/<username>")
+def view_user(username):
+    model.connect_to_db()
+    user_id = model.get_user_by_name(username)
+    posts = model.get_wall_posts(user_id)
+    return render_template("wall.html", the_posts=posts, username=username)
+
+
+
 if __name__ == "__main__":
     app.run(debug = True)
+
+
--- model.py
+++ model.py
@@ -1,5 +1,37 @@
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+# ADMIN_USER="hackbright"
+# ADMIN_PASSWORD=5980025637247534551
+import sqlite3
+
+CONN = None
+DB = None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
+
+def get_user_by_name(username):
+    # connect_to_db()
+
+    query = """SELECT id FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    return DB.fetchone()[0]
 
 def authenticate(username, password):
-    return False
+    query = """SELECT id, username, password FROM users WHERE username = ?"""
+    DB.execute(query, (username, ))
+    row = DB.fetchone()
+  
+    if username == row[1] and hash(password) == hash(row[2]):
+        return row[0] # userid
+    else:
+        return None
+
+# print authenticate("meghan", "urback")
+
+def get_wall_posts(userid):
+    # connect_to_db()
+    query = """SELECT created_at, content FROM wall_posts WHERE owner_id = ?"""
+    DB.execute(query, (userid, ))
+    return DB.fetchall()
